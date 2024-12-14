93bf480efd5e9db09c0c178088555dd1c7994c5d
Kevin KIN-FOO <kkinfoo@smartandconnective.com>
2024-12-15 00:34:05 +0100

    task: guest state rehydration

--- src/modules/order/react/sections/guest/use-guests-section.ts
+++ src/modules/order/react/sections/guest/use-guests-section.ts
@@ -2,8 +2,9 @@ import { useDependencies } from '@ratatouille/modules/app/react/DependenciesProv
 import { GuestForm } from '@ratatouille/modules/order/core/form/guest.form';
 import { OrderingDomainModel } from '@ratatouille/modules/order/core/model/ordering.domain-model';
 import { chooseGuests } from '@ratatouille/modules/order/core/useCases/choose-guests.usecase';
-import { useAppDispatch } from '@ratatouille/modules/store/store';
+import { AppState, useAppDispatch } from '@ratatouille/modules/store/store';
 import { useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 export const useGestsSection = () => {
   function addGuests() {
@@ -41,7 +42,9 @@ export const useGestsSection = () => {
   const dispatch = useAppDispatch();
   const { idProvider } = useDependencies();
   const guestForm = useRef(new GuestForm(idProvider));
-  const [form, setForm] = useState<OrderingDomainModel.Form>({ guests: [] });
+
+  const initialState = useSelector((state: AppState) => state.ordering.form);
+  const [form, setForm] = useState<OrderingDomainModel.Form>(initialState);
 
   return { form, addGuests, removeGuest, updateGuest, changeOrganizer, onNext, isSubmittable };
 };
