c9846ce6ef168f91dd30170ab6fc2ecd6aac847f
Kevin KIN-FOO <kkinfoo@smartandconnective.com>
2024-12-17 00:43:27 +0100

    refactor: meal form

--- src/modules/order/core/form/meal.form.ts
+++ src/modules/order/core/form/meal.form.ts
@@ -17,19 +17,25 @@ import { OrderingDomainModel } from '@ratatouille/modules/order/core/model/order
 // import { useSelector } from 'react-redux';
 
 export class MealForm {
+  private static isMealType(meal: OrderingDomainModel.Meal, type: OrderingDomainModel.MealType): boolean {
+    return meal.type === type;
+  }
+
+  private static hasRequiredAge(meal: OrderingDomainModel.Meal, guest: OrderingDomainModel.Guest): boolean {
+    if (typeof meal.requiredAge === 'number' && guest.age < meal.requiredAge) {
+      return false;
+    }
+    return true;
+  }
+
   getSelectableStarters(
     meals: OrderingDomainModel.Meal[],
     guest: OrderingDomainModel.Guest
   ): OrderingDomainModel.Meal[] {
-    return meals.filter((meal) => {
-      if (meal.type !== OrderingDomainModel.MealType.STARTER) {
-        return false;
-      }
-
-      if (typeof meal.requiredAge === 'number' && guest.age < meal.requiredAge) {
-        return false;
-      }
-      return true;
-    });
+    return meals.filter(
+      (meal) =>
+        MealForm.isMealType(meal, OrderingDomainModel.MealType.STARTER) && //
+        MealForm.hasRequiredAge(meal, guest)
+    );
   }
 }
